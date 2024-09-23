b59b110c32f9feba30de41e1a2d6648fb849e538
Name69 <105518098+Name0518@users.noreply.github.com>
2024-09-23 14:49:19 -0400

    Add files via upload

---
+++ goated budjettech site/app.js
@@ -0,0 +1,13 @@
+const header = document.querySelector('header');
+
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function(){
+    let currentScrollPos = window.scrollY;
+    if(prevScrollPos > currentScrollPos){
+        header.classList.remove('scroll')
+    }else{
+        header.classList.add('scroll');
+    }
+    prevScrollPos = currentScrollPos;
+}
