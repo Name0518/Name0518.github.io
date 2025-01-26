5c8678543831742274131509cbc7b22bcbaac8ed
Name69 <105518098+Name0518@users.noreply.github.com>
2025-01-26 09:58:47 -0500

    New gltf file

--- budjetWebsite/main.js
+++ budjetWebsite/main.js
@@ -17,7 +17,7 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(4, 2, 11);
+camera.position.set(0, 0, 0);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -28,6 +28,7 @@ controls.minPolarAngle = 0.5;
 controls.maxPolarAngle = 1.5;
 controls.autoRotate = false;
 controls.target = new THREE.Vector3(0, 1, 0);
+controls.enableZoom = false;
 controls.update();
 
 const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
@@ -41,16 +42,18 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
+const spotLight = new THREE.SpotLight(0xffffff, 8000, 30, 0.22, 1);
 spotLight.position.set(0, 25, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
-const loader = new GLTFLoader().setPath('public/millennium_falcon/');
-loader.load('robot4.gltf', (gltf) => {
+const loader = new GLTFLoader().setPath('public/robot/');
+loader.load('newrobot.gltf', (gltf) => {
   console.log('loading model');
   const mesh = gltf.scene;
+  gltf.scene.scale.set(5, 5, 5); 
+
 
   mesh.traverse((child) => {
     if (child.isMesh) {
